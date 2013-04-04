a463b861a17433da454fe801f679d26a138c593d
Quico Moya <me@qmoya.com>
2013-04-04 09:48:56 +0200

    Fix duplicate declarations

--- modules/projects/manifests/sg-app-ios.pp
+++ modules/projects/manifests/sg-app-ios.pp
@@ -1,5 +1,5 @@
 class projects::sg-app-ios {
-  boxen::project { 'sg-api-rails':
+  boxen::project { 'sg-app-ios':
     dir 	=> "${boxen::config::srcdir}/stylegeist/app-ios",
     source        => 'stylegeist/app-ios'
   }
