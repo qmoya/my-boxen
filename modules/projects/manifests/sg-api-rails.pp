429150f2371523f70184644abc223ca4aedf890e
Quico Moya <qmoya@Quicos-MacBook-Air.local>
2013-06-21 16:50:55 +0200

    Update

--- modules/projects/manifests/sg-api-rails.pp
+++
@@ -1,7 +0,0 @@
-class projects::sg-api-rails {
-  boxen::project { 'sg-api-rails':
-    dir 	=> "${boxen::config::srcdir}/stylegeist/api-rails",
-    ruby          => '2.0.0',
-    source        => 'stylegeist/api'
-  }
-}
--- modules/projects/manifests/sg-app-ios.pp
+++
@@ -1,6 +0,0 @@
-class projects::sg-app-ios {
-  boxen::project { 'sg-app-ios':
-    dir 	=> "${boxen::config::srcdir}/stylegeist/app-ios",
-    source        => 'stylegeist/app-ios'
-  }
-}
